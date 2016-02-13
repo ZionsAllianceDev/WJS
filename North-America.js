aaf722385e048363237542a7bc1d227865cc697a
ZionsAllianceDev <zionsalliancegaming@gmail.com>
2016-02-13 13:55:50 -0800

    Update North-America.js

--- North-America.js
+++ North-America.js
@@ -1,1 +1,4 @@
-
+var North-America = {
+  Alabama:["Capital:Montgomery","Bird:Yellowhammer","Flower:Camellia"],
+  Alaska:["Capital:Juneau","Bird:Willow ptarmigan","Flower:Alpine-Forget-Me-Not"]
+}
